c6f66895cf7e2c487dc347bd7f0e9cd482ca5dcc
indexedzoom <100958431+indexedzoom@users.noreply.github.com>
2022-03-06 13:27:03 +0000

    Update Audio_Downloader.py

--- Audio_Downloader.py
+++ Audio_Downloader.py
@@ -3,8 +3,8 @@ from mutagen.mp3 import MP3
 from mutagen.id3 import ID3,APIC,error
 from pytube import Playlist,YouTube
 
-audio_path="c:/users/joshu/documents/temp"
-cover_path="c:/users/joshu/documents/temp"
+audio_path=""
+cover_path=""
 blocked_chars='\/:*?"<>|'
 unblocked_chars="⧵∕;＊？“←→⸡"
 count=0
